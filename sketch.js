d9ad3d329d2cc89272f5b524556ed1b36fe98aa5
fa22r <85387362+fa22r@users.noreply.github.com>
2021-06-30 16:44:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+}
+
+function createApples() {
+
+apple = createSprite (random50,350,40,10,10)
+//Add image to apple sprite
+//Scale the sprite if requried
+//Gice the velocity so that the apple move downloads
+//give lifetime to the apple sprite
+
+}
+
+ var select_sprite = Math.round(random(1,2));
+
+if (frameCount % 80 == 0) {
+  if (select_sprite == 1) {
+    // call createApples function
+  }
+else {
+  //call createLeaves function
+ }
+}
+
+
+
+
+
+
+
